cda205dfdd1612908a64f6c95d9b45d4c8e0217f
Anders Kaare Straadt <anders@straadt.dk>
2023-12-28 18:11:02 +0100

    moving repo

--- base64.h
+++
@@ -1,7 +0,0 @@
-#ifndef BASE64_H
-
-char* base64_encode(char* output, uint8_t* input, int n_bytes);
-uint8_t* base64_decode_line(uint8_t* output, char* line);
-
-#define BASE64_H
-#endif
--- sloc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname $0)
-echo -n "controller: "
-cat $(git ls-files '*.c' '*.h' '*.pio' | grep -vF / ) | wc -l
-echo "-----"
-F0=frontend_graphical
-wc -l $(git ls-files '*.c' '*.h' '*.cpp' '*.pio' | grep -vF $F0/imgui | grep -vF $F0/imstb_ | grep -vF $F0/imconfig.h | grep -vF $F0/stb_ds.h)
